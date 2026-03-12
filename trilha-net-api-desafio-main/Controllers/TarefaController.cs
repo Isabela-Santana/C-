ba95258e1ae0f40ace87818d6edffd4a41592004
Isabela Santana <isabelasantanadiaslima@gmail.com>
2026-03-12 16:29:23 -0300

    Add files via upload

---
+++ trilha-net-api-desafio-main/Controllers/TarefaController.cs
@@ -0,0 +1,115 @@
+using Microsoft.AspNetCore.Mvc;
+using TrilhaApiDesafio.Context;
+using TrilhaApiDesafio.Models;
+using System;
+using System.Linq;
+
+namespace TrilhaApiDesafio.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TarefaController : ControllerBase
+    {
+        private readonly OrganizadorContext _context;
+
+        public TarefaController(OrganizadorContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            // Busca o Id no banco utilizando o EF
+            var tarefa = _context.Tarefas.Find(id);
+
+            // Valida o tipo de retorno
+            if (tarefa == null)
+                return NotFound();
+
+            return Ok(tarefa);
+        }
+
+        [HttpGet("ObterTodos")]
+        public IActionResult ObterTodos()
+        {
+            // Busca todas as tarefas no banco utilizando o EF
+            var tarefas = _context.Tarefas.ToList();
+            return Ok(tarefas);
+        }
+
+        [HttpGet("ObterPorTitulo")]
+        public IActionResult ObterPorTitulo(string titulo)
+        {
+            // Busca as tarefas que contenham o titulo recebido por parâmetro
+            var tarefas = _context.Tarefas.Where(x => x.Titulo.Contains(titulo)).ToList();
+            return Ok(tarefas);
+        }
+
+        [HttpGet("ObterPorData")]
+        public IActionResult ObterPorData(DateTime data)
+        {
+            var tarefas = _context.Tarefas.Where(x => x.Data.Date == data.Date).ToList();
+            return Ok(tarefas);
+        }
+
+        [HttpGet("ObterPorStatus")]
+        public IActionResult ObterPorStatus(EnumStatusTarefa status)
+        {
+            var tarefas = _context.Tarefas.Where(x => x.Status == status).ToList();
+            return Ok(tarefas);
+        }
+
+        [HttpPost]
+        public IActionResult Criar(Tarefa tarefa)
+        {
+            if (tarefa.Data == DateTime.MinValue)
+                return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
+
+            // Adiciona a tarefa recebida no EF e salva as mudanças
+            _context.Tarefas.Add(tarefa);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Tarefa tarefa)
+        {
+            var tarefaBanco = _context.Tarefas.Find(id);
+
+            if (tarefaBanco == null)
+                return NotFound();
+
+            if (tarefa.Data == DateTime.MinValue)
+                return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
+
+            // Atualiza as informações da variável tarefaBanco com os dados recebidos
+            tarefaBanco.Titulo = tarefa.Titulo;
+            tarefaBanco.Descricao = tarefa.Descricao;
+            tarefaBanco.Data = tarefa.Data;
+            tarefaBanco.Status = tarefa.Status;
+
+            // Atualiza no EF e salva
+            _context.Tarefas.Update(tarefaBanco);
+            _context.SaveChanges();
+
+            return Ok(tarefaBanco);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            var tarefaBanco = _context.Tarefas.Find(id);
+
+            if (tarefaBanco == null)
+                return NotFound();
+
+            // Remove a tarefa encontrada e salva as mudanças
+            _context.Tarefas.Remove(tarefaBanco);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+    }
+}
